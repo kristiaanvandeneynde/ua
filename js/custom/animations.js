dddf86d8bd42447acde95d6f9b9d1993afea634b
Kristiaan Van den Eynde <magentix@gmail.com>
2013-06-08 16:17:59 +0200

    Added basic scrollbar functionality

--- js/custom/animations.js
+++ js/custom/animations.js
@@ -1,5 +1,11 @@
 (function ($) {
   $(function () {
+    // SOLR search scrollbar
+    $('.solr-result-page').jScrollPane({
+      verticalDragMinHeight: 65,
+      verticalDragMaxHeight: 65,
+      verticalGutter: 22,
+    });
     
     // Sticky tabs
     $('.header-main .tab').mouseenter(function () {
